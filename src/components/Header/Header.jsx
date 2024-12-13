01c6248dc4cd6cc3dd3668e3ec521692bec6e26d
Sam Branham <hotdogsb@gmail.com>
2024-12-12 18:25:43 -0600

    Add Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import "./Header.css";
+import logoImg from "../../assets/logo.svg";
+import avatarImg from "../../assets/avatar.png";
+
+function Header() {
+  return (
+    <header className="header">
+      <img className="header__logo" src={logoImg} alt="WTWR Logo" />
+      <p className="header__info">June 15, New York</p>
+      <button className="header__add-clothes-button">+ Add clothes</button>
+      <div className="header__user-container">
+        <p className="header__username">Terrence Tegegne</p>
+        <img
+          className="header__avatar"
+          src={avatarImg}
+          alt="Terrence Tegegne"
+        />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
