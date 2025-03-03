7c8cfe184668303f0c046aa3b9cd75730dcd9887
Sam Branham <hotdogsb@gmail.com>
2025-03-03 00:25:23 -0600

    fix token authorization

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,10 +1,10 @@
 import { fetchCall } from "./api";
 
 export function signup(body) {
-  return fetchCall("/signup", "POST", JSON.stringify(body));
+  return fetchCall("/signup", "token", "POST", JSON.stringify(body));
 }
 export function signin(body) {
-  return fetchCall("/signin", "POST", JSON.stringify(body));
+  return fetchCall("/signin", "token", "POST", JSON.stringify(body));
 }
 export function checkToken(token) {
   return fetchCall("/users/me", token);
